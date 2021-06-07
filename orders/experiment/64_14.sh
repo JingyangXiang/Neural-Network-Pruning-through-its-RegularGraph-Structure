2dfaa0eb04042b7cee7030f4831d2752b3c084cf
jingyangxiang <jingyangxiang@foxmail.com>
2021-06-07 09:58:54 +0800

    edit sh

--- orders/experiment/64_14.sh
+++ orders/experiment/64_14.sh
@@ -1,9 +1,9 @@
-for ((j=0;i<5;i++))
+for ((i=0;i<5;i++))
 do
-python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_14 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/14/M0.npy
-python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_14 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/14/M57.npy
-python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_14 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/14/M76.npy
-python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_14 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/14/M95.npy
-python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_14 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/14/M107.npy
-python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_14 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/14/M119.npy
+    python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_14 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/14/M0.npy
+    python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_14 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/14/M57.npy
+    python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_14 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/14/M76.npy
+    python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_14 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/14/M95.npy
+    python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_14 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/14/M107.npy
+    python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_14 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/14/M119.npy
 done
--- orders/experiment/64_18.sh
+++ orders/experiment/64_18.sh
@@ -1,8 +1,8 @@
-for ((j=0;i<5;i++))
+for ((i=0;i<5;i++))
 do
-python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_18 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/18/M0.npy
-python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_18 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/18/M79.npy
-python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_18 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/18/M97.npy
-python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_18 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/18/M108.npy
-python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_18 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/18/M119.npy
+    python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_18 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/18/M0.npy
+    python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_18 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/18/M79.npy
+    python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_18 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/18/M97.npy
+    python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_18 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/18/M108.npy
+    python main.py --config configs/experiments/$3-graph-$1.yaml --multigpu $2 --name 64_18 --first-layer-dense --last-layer-dense --set-connection --freeze-subnet --matrix Adjacency/64/18/M119.npy
 done
